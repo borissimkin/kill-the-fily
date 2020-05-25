a3fc69af81a27dfcc45fb39e4252fa655c37268e
boris <simkinbb@gmail.com>
2020-05-25 13:26:51 +0300

    Исправил запись рекордов

--- src/js/records.js
+++ src/js/records.js
@@ -1,25 +1,15 @@
-/** Управление локальным хранилещем **/
+/** Управление локальным хранилещем
+ * кол-во мух: {nickname, time} **/
 
 function addToStorage(nickname, countFlies, time) {
-    window.localStorage.setItem(nickname, JSON.stringify({flies: countFlies, time: time}));
+    window.localStorage.setItem(countFlies, JSON.stringify({nickname: nickname, time: time}));
 
 }
 
 function getBestResultForCurrentNumberFlies(countFlies) {
-    let timesCurrentFlies = []
-    let keys = Object.keys(localStorage);
-    for (let key of keys) {
-        let note = JSON.parse(localStorage.getItem(key));
-        if (note.flies == countFlies) {
-            timesCurrentFlies.push(parseFloat(note.time));
-        }
-
-    }
-
-    timesCurrentFlies.sort(function(a, b){
-        return a - b;
-    });
-    return timesCurrentFlies.pop();
+    let note = JSON.parse(localStorage.getItem(countFlies));
+    if (note)
+        return note.time;
 
 }
 
